61fd325b4b75361ad392a70a0b0f3e41e9075666
Sebastian-Raith <servicesraithsebastian@gmail.com>
2025-02-21 20:37:54 +0100

    Reverted to working version

---
+++ BackendRaith/BackgroundTask/TimedDatabaseUpdate.cs
@@ -0,0 +1,45 @@
+﻿using BackendRaith.Services;
+using System.Timers;
+using Timer = System.Threading.Timer;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BackendRaith.BackgroundTask
+{
+    public class TimedDatabaseUpdate : IHostedService, IDisposable
+    {
+        private readonly IServiceScopeFactory _scopeFactory;
+        private Timer? _timer = null;
+
+        public TimedDatabaseUpdate(IServiceScopeFactory scopeFactory)
+        {
+            _scopeFactory = scopeFactory;
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _timer = new Timer(DoWork, null, TimeSpan.Zero, TimeSpan.FromHours(6));
+            return Task.CompletedTask;
+        }
+
+        private void DoWork(object? state)
+        {
+            using (var scope = _scopeFactory.CreateScope())
+            {
+                var shiftService = scope.ServiceProvider.GetRequiredService<ShiftService>();
+                shiftService.LoadShiftsIntoDB();
+                Console.WriteLine("Shifts loaded into DB");
+            }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _timer?.Change(Timeout.Infinite, 0);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ BackendRaith/DTO/ShiftDTO.cs
@@ -0,0 +1,14 @@
+﻿public class ShiftDTO
+{
+    public int Id { get; set; }
+    public string Date { get; set; }
+    public string ShiftName { get; set; }
+    public string StartTime { get; set; }
+    public string EndTime { get; set; }
+    public string Duration { get; set; }
+    public string Activity { get; set; }
+    public string Department { get; set; }
+    public string AllocationInfo { get; set; }
+    public string DutyType { get; set; }
+    public string Info { get; set; }
+}
